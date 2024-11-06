0acfddb14e284e4c4ad16fdfa65c31938e73183b
fajarpnugroho <temp.fajarpnugroho@gmail.com>
2024-11-06 13:29:09 +0700

    modify github.py adjust request body

--- .ai/auto-review/repository/github.py
+++ .ai/auto-review/repository/github.py
@@ -23,7 +23,10 @@ class GitHub(Repository):
             "body": text,
             "commit_id": commit_id,
             "path" : file_path,
-            "position" : line
+            "start_line": 1,
+            "start_side": "RIGHT",
+            "side":"RIGHT",
+            "line" : line
         }
         response = requests.post(self.__url_add_comment, json = body, headers = headers)
         if response.status_code == 200 or response.status_code == 201:
